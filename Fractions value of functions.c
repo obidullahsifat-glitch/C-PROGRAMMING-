15e65b70bb26b2e8a02f64f607515e932fc7c2e9
obidullahsifat-glitch <obidullahsifat29210@gmail.com>
2026-08-11 04:14:11 +0600

    Add files via upload

---
+++ Fractions value of functions.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+
+printf("First Pair\n\n");
+printf("round and floor\n\n");
+
+printf("same value: round:%.2f,floor:%.2f",round(5.0/4.0),floor(5.0/4.0));
+
+return 0;
+
+
+
+}
