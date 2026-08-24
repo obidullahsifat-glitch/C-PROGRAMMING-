a96b6d4043e4dba269d4ab77af2a010dbb45b367
obidullahsifat-glitch <obidullahsifat29210@gmail.com>
2026-08-24 18:33:20 +0600

    Add files via upload

---
+++ Fibonacci Series nth value.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+ int n;
+ printf("Enter your value");
+ scanf("%d",&n);
+ int a=1;
+ int b=1;
+ int sum=1;
+
+ for(int i=1;i<=n-2;i++)
+ {
+
+ sum=a+b;
+ a=b;
+ b=sum;
+
+
+
+     }
+
+
+printf("%d",sum);
+    return 0;
+
+}
