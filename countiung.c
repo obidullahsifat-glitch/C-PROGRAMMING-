355b340af5ef52dc69f30e75e64dad30841ebece
obidullahsifat-glitch <obidullahsifat29210@gmail.com>
2026-09-01 00:55:22 +0600

    Add files via upload

---
+++ countiung.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+   int i,count;
+   for(i=1;i<=50;i++)
+   {
+   if(i%5==0)
+    {
+   count++;
+    }
+      }
+     printf("Total number count=%d",count);
+     return 0;
+}
