80b374415c78ae82d93071f0852414d356914459
obidullahsifat-glitch <obidullahsifat29210@gmail.com>
2026-09-01 00:47:23 +0600

    Add files via upload

--- Summation and Counting.c
+++ Summation and Counting.c
@@ -2,15 +2,17 @@
 int main()
 {
   int n;
+  int sum;
   printf("Enter your value");
   scanf("%d",&n);
   for(int i=1;i<=n;i++)
   {
       if(i%2!=0)
-      {
+      {   sum=sum+i;
           printf("%d\n",i);
       }
   }
+  printf("Sum=%d",sum);
 
 
     return 0;
